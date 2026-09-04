210823d52db930aef2bdcae681051f51ac8d4802
martingerardoc <martingerardoc56@gmail.com>
2026-09-04 15:39:41 -0300

    contacts created

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-routes.get('/', (req, res) => {
+router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+router.use('/contacts', require('./contacts'))
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', requie('./routes'));
+app.use(bodyParser.json());
+app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-requested-With, Contenc-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+ if(err) {
+    console.log(err);
+ }
+ else {
+    app.listen(port, () => {console.log(`Database is listening and node Running on port ${port}`)});
+ }
+});
 
 app.listen(port, () => {console.log(`Running on port ${port}`)});
